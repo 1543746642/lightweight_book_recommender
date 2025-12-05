3af1f7a8abed4641cbe76b9d6620edbf06a0c012
jiaayu <1543746642jiayu@gmail.com>
2025-12-05 12:31:57 +0800

    commit

--- src/ai_demo_testing/utils/log.py
+++ src/ai_demo_testing/utils/log.py
@@ -6,7 +6,6 @@ import sys
 # %(message)s: 日志内容
 # %(pathname)s: 完整的文件路径名
 # %(lineno)d: 产生日志调用的行号
-# (注意：这里的格式是许多IDE默认识别为跳转链接的关键！)
 LOG_FORMAT = '%(pathname)s:%(lineno)d: [%(levelname)s] - %(message)s'
 
 
@@ -23,11 +22,10 @@ def setup_logging():
     # sys.stderr 是默认的控制台输出流
     console_handler = logging.StreamHandler(sys.stderr)
 
-    # 设置 Handler 的输出级别（例如，只在控制台输出 INFO 及以上的日志）
-    # 这里为了演示，我们设置为 DEBUG
+    # 设置 Handler 的输出级别
     console_handler.setLevel(logging.DEBUG)
 
-    # 创建格式化器 (Formatter) 并应用我们定义的格式
+    # 创建格式化器 (Formatter) 并应用
     formatter = logging.Formatter(LOG_FORMAT)
 
     # 将格式化器设置给处理器
@@ -38,23 +36,3 @@ def setup_logging():
         logger.addHandler(console_handler)
 
 
-def main():
-    """主函数，演示不同级别的日志输出。"""
-
-    # 确保 logging 已配置
-    setup_logging()
-
-    logging.debug("这是一条DEBUG级别的消息，用于详细的调试信息。")
-    logging.info("这是一条INFO级别的消息，程序正常运行的关键信息。")
-
-    try:
-        result = 10 / 0  # 制造一个错误
-    except ZeroDivisionError as e:
-        # 使用 exc_info=True 可以将当前的异常信息也输出到日志中
-        logging.error("程序执行时发生了一个错误。", exc_info=True)
-
-    logging.warning("这是一条WARNING级别的消息，需要注意但不是致命问题。")
-    
-
-if __name__ == '__main__':
-    main()
